428e618dd2b6c1506a0fab2b35b77cfc4faf6139
NCrashed <NCrashed@gmail.com>
2016-02-06 23:23:22 +0300

    blit texture and refactor

--- src/client/Game.hs
+++ src/client/Game.hs
@@ -3,10 +3,23 @@ module Game(
   , Game(..)
   ) where
 
+import Control.Wire 
+import Prelude hiding ((.), id)
+import Linear 
+
+import Consts
 import Game.Core 
 import Game.Data 
 
 import Render.Texture
 
+import Game.GoreAndAsh.SDL 
+
 mainWire :: AppWire a (Maybe Game)
-mainWire = pure $ Just $ Game False
+mainWire = (<|> pure Nothing) $ proc _ -> do 
+  (t, _) <- dynamicTexture mainWindowName RGBA8888 (V2 600 800) drawRectangle -< ()
+  blitTexture mainWindowName Nothing Nothing -< t
+  returnA -< Just $ Game False
+
+drawRectangle :: AppWire a ()
+drawRectangle = proc _ -> returnA -< ()
--- src/client/Render/Texture.hs
+++ src/client/Render/Texture.hs
@@ -1,5 +1,6 @@
 module Render.Texture(
     dynamicTexture
+  , blitTexture
   ) where
 
 import Control.Wire 
@@ -8,9 +9,42 @@ import Data.Text
 import Linear 
 
 import Game.GoreAndAsh
-import Game.GoreAndAsh.SDL 
+import Game.GoreAndAsh.SDL as SDL
 
 import Game.Core 
+
+-- | Execute wire with renderer for given window.
+-- 
+-- Note: inhibits until Window isn't found.
+withRenderer :: MonadSDL m 
+  => Text -- ^ Name of window
+  -> (Renderer -> GameWire m a b) -- ^ Action with renderer
+  -> GameWire m a b
+withRenderer wname rw = go 
+  where
+  go = mkGen $ \_ _ -> do 
+    mwr <- sdlGetWindowM wname
+    case mwr of 
+      Nothing -> return (Left (), go)
+      Just (_, r) -> return (Left (), rw r)
+
+-- | Execute wire with renderer for given window.
+-- 
+-- Note: inhibits until Window isn't found.
+withRendererM :: MonadSDL m 
+  => Text -- ^ Name of window
+  -> (Renderer -> GameMonadT m r) -- ^ Intialization of resource
+  -> (r -> GameWire m a b) -- ^ Wire with resource
+  -> GameWire m a b
+withRendererM wname mr rw = go 
+  where
+  go = mkGen $ \_ _ -> do 
+    mwr <- sdlGetWindowM wname
+    case mwr of 
+      Nothing -> return (Left (), go)
+      Just (_, r) -> do 
+        r' <- mr r 
+        return (Left (), rw r')
 
 -- | Wraps rendering wire to render into texture
 dynamicTexture :: 
@@ -19,18 +53,23 @@ dynamicTexture ::
   -> V2 Int -- ^ Size for texture
   -> AppWire a b -- ^ Rendering wire that output is directed to texture
   -> AppWire a (Texture, b) -- ^ Result is texture and rendering wire output
-dynamicTexture wname pf sz drawWire = initTex
+dynamicTexture wname pf sz drawWire = withRendererM wname initTex renderTex
   where
-  initTex = mkGen $ \_ _ -> do 
-    mwr <- sdlGetWindowM wname
-    case mwr of 
-      Nothing -> return (Left (), initTex)
-      Just (_, r) -> do
-        t <- createTexture r pf TextureAccessTarget (fromIntegral <$> sz) 
-        return (Left (), renderTex t)
+  initTex r = createTexture r pf TextureAccessTarget (fromIntegral <$> sz)
 
   renderTex t = proc a -> do  
     liftGameMonad $ glBindTexture t -< ()
     b <- drawWire -< a
     liftGameMonad $ glUnbindTexture t  -< ()
     returnA -< (t, b)
+
+-- | Blits input texture into window surface
+blitTexture :: 
+     Text -- ^ Name of window
+  -> Maybe (Rectangle Int) -- ^ Source region, 'Nothing' is full region
+  -> Maybe (Rectangle Int) -- ^ Destination region, 'Nothing' is full region
+  -> AppWire Texture ()
+blitTexture wname sr dr = withRenderer wname $ \r -> proc t -> do 
+  liftGameMonad1 (\t -> SDL.copy r t (rconv sr) (rconv dr)) -< t 
+  where 
+    rconv = (fmap fromIntegral <$>)
