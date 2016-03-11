1b971e6b436e0a15cad37406213bba0eb7565c4e
NCrashed <ncrashed@gmail.com>
2016-03-11 15:15:21 +0300

    Disable FPS block for Windows

--- src/shared/FPS.hs
+++ src/shared/FPS.hs
@@ -3,25 +3,17 @@ module FPS(
   , waitFPSBound
   ) where
 
-import GHC.Event
 import Control.Concurrent
 import Control.Monad 
 
 type FPSBound = MVar ()
 
+-- Temporaly disabled, as Windows doesn't have GHC.Events 
+
 -- | Creates mvar that fills periodically with given fps
 makeFPSBounder :: Int -> IO FPSBound
-makeFPSBounder fps = do
-  v <- newEmptyMVar 
-  tm <- getSystemTimerManager
-  let t = ceiling ((1000000 :: Double) / fromIntegral fps)
-  callback v tm t
-  return v
-  where
-    callback v tm t = do 
-      putMVar v ()
-      void $ registerTimeout tm t $ callback v tm t
+makeFPSBounder fps = newEmptyMVar
 
 -- | Wait until next FPS value is reached when the function unblocks
 waitFPSBound :: FPSBound -> IO ()
-waitFPSBound = takeMVar 
+waitFPSBound _ = return ()
